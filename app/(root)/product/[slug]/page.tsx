b05c012d804194025c93990118a3f3f3ca837b41
Abid Hussain <orasundev@gmail.com>
2025-04-27 02:46:30 -0700

    11-Implement-Add-To-Cart

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -10,8 +10,8 @@ import ProductGallery from '@/components/shared/product/product-gallery'
 import { Separator } from '@/components/ui/separator'
 import ProductSlider from '@/components/shared/product/product-slider'
 import Rating from '@/components/shared/product/rating'
-import BrowsingHistoryList from '@/components/shared/browsing-history-list'
-import AddToBrowsingHistory from '@/components/shared/product/add-to-browsing-history'
+import AddToCart from '@/components/shared/product/add-to-cart'
+import { generateId, round2 } from '@/lib/utils'
 
 export async function generateMetadata(props: {
   params: Promise<{ slug: string }>
@@ -49,7 +49,6 @@ export default async function ProductDetails(props: {
 
   return (
     <div>
-      <AddToBrowsingHistory id={product._id} category={product.category} />
       <section>
         <div className='grid grid-cols-1 md:grid-cols-5  '>
           <div className='col-span-2'>
@@ -79,6 +78,7 @@ export default async function ProductDetails(props: {
                 </div>
               </div>
             </div>
+
             <div>
               <SelectVariant
                 product={product}
@@ -94,6 +94,7 @@ export default async function ProductDetails(props: {
               </p>
             </div>
           </div>
+
           <div>
             <Card>
               <CardContent className='p-4 flex flex-col  gap-4'>
@@ -109,6 +110,27 @@ export default async function ProductDetails(props: {
                 ) : (
                   <div className='text-destructive text-xl'>Out of Stock</div>
                 )}
+
+                {/* {// point 10 not mached code see th issue later } */}
+                {product.countInStock !== 0 && (
+                  <div className='flex justify-center items-center'>
+                    <AddToCart
+                      item={{
+                        clientId: generateId(),
+                        product: product._id,
+                        countInStock: product.countInStock,
+                        name: product.name,
+                        slug: product.slug,
+                        category: product.category,
+                        price: round2(product.price),
+                        quantity: 1,
+                        image: product.images[0],
+                        size: size || product.sizes[0],
+                        color: color || product.colors[0],
+                      }}
+                    />
+                  </div>
+                )}
               </CardContent>
             </Card>
           </div>
@@ -120,10 +142,6 @@ export default async function ProductDetails(props: {
           products={relatedProducts.data}
           title={`Best Sellers in ${product.category}`}
         />
-      </section>
-
-      <section>
-        <BrowsingHistoryList className='mt-10' />
       </section>
     </div>
   )
--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,10 @@
-import { ProductInputSchema } from '@/lib/validator'
+///import { ProductInputSchema } from '@/lib/validator'
 import { z } from 'zod'
+import {
+  CartSchema,
+  OrderItemSchema,
+  ProductInputSchema,
+} from '@/lib/validator'
 
 export type IProductInput = z.infer<typeof ProductInputSchema>
 
@@ -17,3 +22,6 @@ export type Data = {
     isPublished: boolean
   }[]
 }
+
+export type OrderItem = z.infer<typeof OrderItemSchema>
+export type Cart = z.infer<typeof CartSchema>
