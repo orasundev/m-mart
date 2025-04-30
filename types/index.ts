3f0634ba2f0db7d706fc259fc3ea0eed80dfa82f
Abid Hussain <orasundev@gmail.com>
2025-04-29 23:46:52 -0700

    17-create-checkout-page

--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,7 @@ import {
   CartSchema,
   OrderItemSchema,
   ProductInputSchema,
+  ShippingAddressSchema,
   UserInputSchema,
   UserSignInSchema,
   UserSignUpSchema,
@@ -29,6 +30,7 @@ export type Data = {
 
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
+export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
 
 // user
 export type IUserInput = z.infer<typeof UserInputSchema>
