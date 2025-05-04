fb2fd3a17f3334f6a33bb2d68cdca48af0ff2d09
Abid Hussain <orasundev@gmail.com>
2025-05-04 00:31:46 -0700

    25-create-search-page

--- components/shared/header/search.tsx
+++ components/shared/header/search.tsx
@@ -9,9 +9,10 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 import { APP_NAME } from '@/lib/constants'
+import { getAllCategories } from '@/lib/actions/product.actions'
 
-const categories = ['men', 'women', 'kids', 'accessories']
 export default async function Search() {
+  const categories = await getAllCategories()
   return (
     <form action='/search' method='GET' className='flex  items-stretch h-9 '>
       <Select name='category'>
